03a9f565e6b2f96b004625f939bf642452dddda2
Fabio Posada <fabioposm@gmail.com>
2019-06-06 12:18:45 -0500

    first commit

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = express();
 
 const greeting = require('./routes/greeting')
 
+app.get('/', (req, res) => res.send('Ok'))
 app.post('/greeting', greeting.hello);
-app.post('/greeting/', greetings.goodbye)
+//app.post('/greeting/', greetings.goodbye)
 
 app.listen(3000, function() {
   console.log('Listening on port 3000');
